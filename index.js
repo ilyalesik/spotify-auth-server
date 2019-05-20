fd5e24f0d48831f5f58ebcbcbbbc48e20d5f83c7
Ilia Lesik <ilialesik@gmail.com>
2019-05-20 11:08:32 +0300

    Implement refresh token support

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const fetch = require('node-fetch');
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
 
+const refreshToken = process.env.REFRESH_TOKEN;
+
 const encodedClientCredentials = (new Buffer(clientId + ":" + clientSecret)).toString('base64');
 
 const postOptions = {
     method: "post",
-    body: "grant_type=client_credentials",
+    body: refreshToken ? ("grant_type=refresh_token&refresh_token=" + refreshToken) : "grant_type=client_credentials",
     headers: {
         Accept: "application/json, application/xml, text/plain, text/html, *.*",
         "Content-Type": "application/x-www-form-urlencoded",
